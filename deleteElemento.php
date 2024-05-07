2598eb88c3c187b49de39f6b34e20e972440fa1f
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-07 14:58:14 +0200

    deleteElementos

--- deleteElemento.php
+++ deleteElemento.php
@@ -5,18 +5,19 @@ header('Content-Type: application/json');
 
 include('./connection.php');
 
-if (!isset($_POST['id_api'])) {
-  $response = ['success' => false, 'message' => 'id_api is required'];
+if (!isset($_POST['id_usuario']) || !isset($_POST['id_api'])) {
+  $response = ['success' => false, 'message' => 'id_usuario and id_api are required'];
 } else {
+  $id_usuario = $_POST['id_usuario'];
   $id_api = $_POST['id_api'];
 
-  $query = "DELETE from elementos where id_api='$id_api'";
+  $query = "DELETE FROM elementos_usuario WHERE id_usuario='$id_usuario' AND id_elemento IN (SELECT id FROM elementos WHERE id_api='$id_api')";
   $result = mysqli_query($connect, $query);
 
   if ($result) {
-    $response = ['success' => true, 'message' => 'Elemento deleted successfully'];
+    $response = ['success' => true, 'message' => 'Elemento deleted successfully for the user'];
   } else {
-    $response = ['success' => false, 'message' => 'Something went wrong while deleting the publicación'];
+    $response = ['success' => false, 'message' => 'Something went wrong while deleting the elemento_usuario relationship'];
   }
 }
 
