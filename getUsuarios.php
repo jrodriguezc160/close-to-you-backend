caea5074e1eb40ed69e5940f2cd0c67270959f5a
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-19 13:44:47 +0200

    control para el nuevo modelo

--- getUsuarios.php
+++ getUsuarios.php
@@ -5,28 +5,35 @@ header('Content-Type: application/json');
 
 include('./connection.php');
 
+$response = [];
+
 $query = 'SELECT * FROM usuarios';
 $result = mysqli_query($connect, $query);
 
-$usuarios = array();
+if ($result) {
+    $usuarios = [];
 
-while ($row = mysqli_fetch_array($result)) {
-  $usuarios[] = array (
-    'id' => $row['id'],
-    'nombre_mostrado' => $row['nombre_mostrado'],
-    'usuario' => $row['usuario'],
-    'email' => $row['email'],
-    'nombre' => $row['nombre'],
-    'apellido1' => $row['apellido1'],
-    'apellido2' => $row['apellido2'],
-    'fecha_nacimiento' => $row['fecha_nacimiento'],
-    'foto_perfil' => $row['foto_perfil'],
-    'banner' => $row['banner'],
-    'descripcion' => $row['descripcion']
-  );
+    while ($row = mysqli_fetch_assoc($result)) {
+        $usuarios[] = [
+            'id' => $row['id'],
+            'nombre_mostrado' => $row['nombre_mostrado'],
+            'usuario' => $row['usuario'],
+            'email' => $row['email'],
+            'nombre' => $row['nombre'],
+            'apellido1' => $row['apellido1'],
+            'apellido2' => $row['apellido2'],
+            'fecha_nacimiento' => $row['fecha_nacimiento'],
+            'foto_perfil' => $row['foto_perfil'],
+            'banner' => $row['banner'],
+            'descripcion' => $row['descripcion']
+        ];
+    }
+
+    $response = ['success' => true, 'data' => $usuarios];
+} else {
+    $response = ['success' => false, 'message' => 'Error fetching users'];
 }
-
-$response = ['success' => true, 'data' => $usuarios];
 
 echo json_encode($response);
 exit();
+?>
--- getUsuariosSeguidos.php
+++ getUsuariosSeguidos.php
@@ -5,30 +5,36 @@ header('Content-Type: application/json');
 
 include('./connection.php');
 
-if (!isset($_GET['id_usuario'])) {
-  $response = ['success' => false, 'message' => 'id_usuario is required'];
-} else {
-  $id_usuario = $_GET['id_usuario'];
+$response = [];
 
-  $query = "SELECT usuarios.id, usuarios.nombre_mostrado, usuarios.usuario, usuarios.descripcion, usuarios.foto_perfil
-            FROM siguiendo
-            INNER JOIN usuarios ON siguiendo.usuario_seguido = usuarios.id
-            WHERE siguiendo.seguidor='$id_usuario'";
-  $result = mysqli_query($connect, $query);
-  
-  $usuariosSeguidos = array();
-  
-  while ($row = mysqli_fetch_array($result)) {
-    $usuariosSeguidos[] = array (
-    'id' => $row['id'],
-    'nombre_mostrado' => $row['nombre_mostrado'],
-    'usuario' => $row['usuario'],
-    'descripcion' => $row['descripcion'],
-    'foto_perfil' => $row['foto_perfil']
-    );
-  }
-  
-  $response = ['success' => true, 'data' => $usuariosSeguidos];
+if (!isset($_GET['id_usuario'])) {
+    $response = ['success' => false, 'message' => 'id_usuario is required'];
+} else {
+    $id_usuario = $_GET['id_usuario'];
+
+    $query = "SELECT usuarios.id, usuarios.nombre_mostrado, usuarios.usuario, usuarios.descripcion, usuarios.foto_perfil
+              FROM siguiendo
+              INNER JOIN usuarios ON siguiendo.usuario_seguido = usuarios.id
+              WHERE siguiendo.seguidor=?";
+    
+    $stmt = mysqli_prepare($connect, $query);
+    mysqli_stmt_bind_param($stmt, 's', $id_usuario);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    $usuariosSeguidos = [];
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        $usuariosSeguidos[] = [
+            'id' => $row['id'],
+            'nombre_mostrado' => $row['nombre_mostrado'],
+            'usuario' => $row['usuario'],
+            'descripcion' => $row['descripcion'],
+            'foto_perfil' => $row['foto_perfil']
+        ];
+    }
+
+    $response = ['success' => true, 'data' => $usuariosSeguidos];
 }
 
 echo json_encode($response);
