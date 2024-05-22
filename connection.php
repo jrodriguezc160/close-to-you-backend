d6ed324158b7b0e7c4e7ffd5962752b985d048cc
jrodriguezc160 <jrcortes02102005@educarex.es>
2024-05-22 11:15:41 +0200

    asdfasdf

--- connection.php
+++ connection.php
@@ -1,14 +1,10 @@
 <?php
 
-// $host = 'fdb1034.awardspace.net';  // Host de la base de datos proporcionado por tu proveedor
-// $user = '4472287_closetoyou';      // Usuario de la base de datos proporcionado
-// $password = 'Camavinga12!';  // Sustituye 'your_password_here' por tu contraseña real
-// $db = '4472287_closetoyou';        // Nombre de la base de datos proporcionado
+$host = 'fdb1034.awardspace.net';  // Host de la base de datos proporcionado por tu proveedor
+$user = '4472287_closetoyou';      // Usuario de la base de datos proporcionado
+$password = 'Camavinga12!';  // Sustituye 'your_password_here' por tu contraseña real
+$db = '4472287_closetoyou';        // Nombre de la base de datos proporcionado
 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'close-to-you';
 
 $connect = new mysqli($host, $user, $password, $db);
 
